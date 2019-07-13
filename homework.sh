66481a6f38d2bfb08ee5534baec7fcbffcecb60a
Alexey Tamarevskiy <mirror@inetra.ru>
2019-07-13 10:06:24 +0700

    Fix ranges

--- homework.sh
+++ homework.sh
@@ -11,9 +11,9 @@ if [ $x -le 5 ]; then
     echo "$d `date -R`"
 elif [ $x -le 12 ]; then
     echo "$a `date -R`"
-elif [ $x -le 12 ]; then
+elif [ $x -le 17 ]; then
     echo "$b `date -R`"
-elif [ $x -le 12 ]; then
+elif [ $x -le 22 ]; then
     echo "$c `date -R`"
 else
     echo "$d `date -R`"
